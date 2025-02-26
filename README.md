9032d3e3a33a4e197e10f7cf37c2e83765865a42
Travis Reeder <treeder@gmail.com>
2025-02-26 00:20:37 +0000

    add code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # betterstack
+
 Betterstack JavaScript libraries. 
+
+## Install
+
+```sh
+npm install treeder/betterstack
+```
+
+## Usage
+
+```js
+let bsLogger = new BetterstackLogger({host: "abc.betterstackdata.com", token: "bstoken"})
+bsLogger.log("hello world!")
+bsLogger.log({message: "hello!"})
+bsLogger.log(err)
+bsLogger.log("uh oh", err)
+bsLogger.log("This is some data:", {name: "John Wick"})
+```
---
+++ betterstack.js
@@ -0,0 +1,68 @@
+import api from "api"
+
+export class BetterstackLogger {
+  constructor(options = {}) {
+    if (!options.url) {
+      if (options.host) {
+        options.url = `https://${options.host}`
+      } else {
+        throw new Error("BetterstackLogger: missing url")
+      }
+    }
+    if (!options.token) {
+      throw new Error("BetterstackLogger: missing token")
+    }
+    this.options = options
+    this.messages = [] // for sending in batches
+  }
+  log(...params) {
+    console.log(...params)
+    let data = {
+      level: "info"
+    }
+    let err = null
+    // for (let p of params) {
+    //   if (p instanceof Error) {
+    //     err = p
+    //   }
+    // }
+    let last = params[params.length - 1]
+    if (last instanceof Error) {
+      err = last
+      params.pop()
+    } else if (typeof last == "object") {
+      // then data object
+      data.data = last
+      params.pop()
+    }
+    if (err) {
+      data.message = err.message
+      data.level = "error" // betterstack entry
+      data.error = {
+        stack: err.stack,
+        status: err.status,
+        cause: err.cause,
+      }
+    }
+    if (params.length > 0) { // already popped
+      data.message = params.join(" ")
+    }
+    if (!data.message) data.message = "no message"
+    // console.log("betterstack data:", data)
+    this.messages.push(data)
+  }
+  async flush() {
+    let body = this.messages
+    // console.log(body)
+    let r = await api(`${this.options.url}`, {
+      method: "POST",
+      headers: { "Authorization": `Bearer ${this.options.token}` },
+      body: body,
+      raw: true,
+    })
+    if (!r.ok) {
+      console.error("BETTERSTACK ERROR:", r.status, r.statusText)
+    }
+  }
+
+}
